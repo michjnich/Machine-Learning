f6ecd6db8e4ea2b2ccec09116f1a360aa8f59e2c
Michael J. Nicholson <mjnicholson@outlook.com>
2017-08-18 15:00:58 +0200

    FIxed the NN predict

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,17 +21,18 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-temp_X = [ones(m, 1) X];
+a1 = [ones(m, 1) X];
 
-z2 = temp_X * Theta1';
-z2 = [ones(m,1) z2];
-a2 = sigmoid(z2)';
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(m,1) a2];
 
-z3 = a2' * Theta2';
+z3 = a2 * Theta2';
 a3 = sigmoid(z3);
 
-[max_a3, max_a3_idx] = max(a3');
-p = max_a3_idx';
+
+[max_a3, max_a3_idx] = max(a3, [], 2);
+p = max_a3_idx;
 
 % =========================================================================
 
