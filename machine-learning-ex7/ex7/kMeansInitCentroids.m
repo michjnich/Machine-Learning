09409e7404be237e8f8b31d525f3bfd298fb5214
urbanespaceman <mjnicholson@outlook.com>
2017-09-12 13:34:42 +0200

    Test on own image

--- machine-learning-ex7/ex7/kMeansInitCentroids.m
+++ machine-learning-ex7/ex7/kMeansInitCentroids.m
@@ -1,5 +1,5 @@
 function centroids = kMeansInitCentroids(X, K)
-%KMEANSINITCENTROIDS This function initializes K centroids that are to be 
+%KMEANSINITCENTROIDS This function initializes K centroids that are to be
 %used in K-Means on the dataset X
 %   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
 %   used with the K-Means on the dataset X
@@ -13,14 +13,13 @@ centroids = zeros(K, size(X, 2));
 %               the dataset X
 %
 
+% Randomly reorder the indicies
+randidx=randperm(size(X,1));
 
-
-
-
-
+% Take the first K random points as centroids
+centroids=X(randidx(1:K), :);
 
 
 % =============================================================
 
 end
-
