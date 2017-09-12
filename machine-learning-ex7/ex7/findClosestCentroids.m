b977c6ad8bf0aed876de94cf7a434a0ea0dae678
urbanespaceman <mjnicholson@outlook.com>
2017-09-12 13:23:49 +0200

    Compute and plot centroids

--- machine-learning-ex7/ex7/findClosestCentroids.m
+++ machine-learning-ex7/ex7/findClosestCentroids.m
@@ -1,7 +1,7 @@
 function idx = findClosestCentroids(X, centroids)
 %FINDCLOSESTCENTROIDS computes the centroid memberships for every example
 %   idx = FINDCLOSESTCENTROIDS (X, centroids) returns the closest centroids
-%   in idx for a dataset X where each row is a single example. idx = m x 1 
+%   in idx for a dataset X where each row is a single example. idx = m x 1
 %   vector of centroid assignments (i.e. each entry in range [1..K])
 %
 
@@ -15,19 +15,22 @@ idx = zeros(size(X,1), 1);
 % Instructions: Go over every example, find its closest centroid, and store
 %               the index inside idx at the appropriate location.
 %               Concretely, idx(i) should contain the index of the centroid
-%               closest to example i. Hence, it should be a value in the 
+%               closest to example i. Hence, it should be a value in the
 %               range 1..K
 %
 % Note: You can use a for-loop over the examples to compute this.
 %
 
+% Following ex7 turotial and iterating over the centroids for a faster solution
 
+distance = zeros(size(X,1), K);
 
+for i = 1:K
+   distance(:,i)=sum(bsxfun(@(x,y) x-y, X, centroids(i,:)) .^ 2, 2);
+end
 
-
-
+[dist, idx] = min(distance, [], 2);
 
 % =============================================================
 
 end
-
