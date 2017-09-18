2fad80ef1fe9cfbd3e66726761af8a4de736f447
Michael J. Nicholson <mjnicholson@outlook.com>
2017-09-18 21:04:37 +0200

    Collaborative filtering and gradient

--- machine-learning-ex8/ex8/cofiCostFunc.m
+++ machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,20 +40,9 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+J = (1 / 2) * sum(((X * Theta' - Y) .^ 2)(R == 1));
+X_grad = ((X * Theta' - Y) .* R) * Theta;
+Theta_grad = ((X * Theta' - Y) .* R)' * X;
 
 % =============================================================
 
